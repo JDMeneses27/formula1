e4b7ad8c048f7de1a2ea6cca88befdf168617150
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-05-05 03:11:29 -0500

    feat: :sparkles: Creacion Validacion User/Admin con Js

--- src/js/login.js
+++ src/js/login.js
@@ -1,13 +1,30 @@
-const loginForm = document.querySelector('#loginForm')
-loginForm.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
-    if(!validUser){
-        return alert('Usuario y/o contraseña incorrectos')
+// const loginForm = document.querySelector('#loginForm')
+// loginForm.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+//     const email = document.querySelector('#email').value
+//     const password = document.querySelector('#password').value
+//     const Users = JSON.parse(localStorage.getItem('users')) || []
+//     const validUser = Users.find(user => user.email === email && user.password === password)
+//     if(!validUser){
+//         return alert('Usuario y/o contraseña incorrectos')
+//     }
+//     alert(`Bienvenido ${validUser.user}`)
+//     window.location.href = "../html/index.html"
+// })
+
+import { users } from './roles.js';
+
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        window.location.href = `src/html/index.html?role=${user.role}`;
+    } else {
+        alert('Credenciales inválidas. Por favor, intente nuevamente.');
     }
-    alert(`Bienvenido ${validUser.user}`)
-    window.location.href = "../html/index.html"
-})
+});
---
+++ src/js/roles.js
@@ -0,0 +1,12 @@
+export const users = [
+    {
+        email: 'campus@gmail.com',
+        password: 'admin123',
+        role: 'admin',
+    },
+    {
+        email: 'user@gmail.com',
+        password: 'user123',
+        role: 'user',
+    },
+];
